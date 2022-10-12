ca398bc1d3b179af9508e457fca217c4331850fa
luan <luanbatista904@outlook.com>
2022-10-11 22:19:57 -0400

    Adicionando nova versão do codigo

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,5 +1,24 @@
-let menu = document.querySelector('#menu');
+const menu = document.querySelector('#menu');
+const menuIcon = document.querySelector('#menuIcon');
 function showMenu() {
-  menu.classList.toggle('showMenu');
+  menu.classList.toggle('show');
+  changeMenuIcon();
 }
+menuIcon.addEventListener('click', showMenu);
 
+addEventListener('resize', ()=>{
+  if(window.innerWidth > 768){
+    menu.classList.remove('show');
+    changeMenuIcon();
+  }
+});
+
+function changeMenuIcon(){
+  if(menu.classList.contains('show')){
+    menuIcon.src = 'assets/icons/iconCloseMenu.svg';
+    menuIcon.title = 'Fechar menu'
+  }else{
+    menuIcon.src = 'assets/icons/iconMenu.svg';
+    menuIcon.title = 'Mostrar menu'
+  }
+}
